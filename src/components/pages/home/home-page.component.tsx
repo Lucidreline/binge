c1c4bdaa31b83acee4bbb9e492689ed6e14de690
Lucidreline <techservs@pm.me>
2020-07-13 16:20:08 -0700

    Home page now has different lists of shows.

--- src/components/pages/home/home-page.component.tsx
+++ src/components/pages/home/home-page.component.tsx
@@ -6,10 +6,10 @@ import GliderList from '../../glider-list/glider-list.component';
 interface IProps {}
 
 interface IState {
-  trendingShows: IResult[];
+  showLists: IShowList[];
 }
 
-interface IResult {
+interface IShow {
   poster_path: string;
   id: number;
   [key: string]: any;
@@ -17,31 +17,66 @@ interface IResult {
 
 interface IResponce {
   [key: string]: any;
-  results: IResult[];
+  results: IShow[];
+}
+
+interface IShowList {
+  name: string;
+  searchBy: string;
+  shows: IShow[];
 }
 
 class HomePage extends Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
     this.state = {
-      trendingShows: [],
+      showLists: [
+        {
+          name: 'Popular',
+          searchBy: 'popular',
+          shows: [],
+        },
+        {
+          name: 'Top Rated',
+          searchBy: 'top_rated',
+          shows: [],
+        },
+        {
+          name: 'On Air Today',
+          searchBy: 'airing_today',
+          shows: [],
+        },
+        {
+          name: 'On Air Right Now',
+          searchBy: 'on_the_air',
+          shows: [],
+        },
+      ],
     };
   }
 
-  componentDidMount() {
-    fetch(
-      `https://api.themoviedb.org/3/trending/tv/day?api_key=${env.TMDB_KEY}`
-    )
-      .then((res) => res.json())
-      .then((resJson: IResponce) => {
-        this.setState({ trendingShows: resJson.results });
-      });
+  async componentDidMount() {
+    let tempState = this.state;
+    for (let i = 0; i < tempState.showLists.length; i++) {
+      await fetch(
+        `https://api.themoviedb.org/3/tv/${tempState.showLists[
+          i
+        ].searchBy.toLowerCase()}?api_key=${env.TMDB_KEY}&language=en-US`
+      )
+        .then((res) => res.json())
+        .then((resJson: IResponce) => {
+          tempState.showLists[i].shows = resJson.results;
+        });
+      this.setState(tempState);
+    }
   }
 
   render() {
     return (
       <div className='home-page'>
-        <GliderList title='Shows' results={this.state.trendingShows} />
+        {this.state.showLists.map((list, index) => (
+          <GliderList title={list.name} results={list.shows} key={index} />
+        ))}
       </div>
     );
   }
